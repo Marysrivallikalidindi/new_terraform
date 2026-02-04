bb5377c3258b27470d4cc6eb631729f903d2d33c
Marysrivallikalidindi <srivalli.kalidindi2004@gmail.com>
2026-02-04 14:35:00 +0530

    multiple subnets with one vpc

---
+++ multiplesubnet/main.tf
@@ -0,0 +1,18 @@
+resource "aws_vpc" "mydemovpc" {
+    cidr_block = var.network_info.vpccidr
+    tags = {
+      Name = var.network_info.vpcname
+
+    }
+  
+}
+resource "aws_subnet" "subnets" {
+    count = length(var.network_info.subnets[0].subnetcidr)
+    vpc_id = aws_vpc.mydemovpc.id
+    cidr_block = var.network_info.subnets[0].subnetcidr[count.index]
+    availability_zone = var.network_info.subnets[0].subnetsaz[count.index]
+    tags = {
+      Name = var.network_info.subnets[0].subnetname[count.index]
+    }
+  
+}
