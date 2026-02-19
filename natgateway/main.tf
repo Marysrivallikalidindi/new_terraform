61ceb72a01c64749ac03519ff6fc37ed4ddb376b
Marysrivallikalidindi <srivalli.kalidindi2004@gmail.com>
2026-02-19 23:48:27 +0530

    main.tf file modified

--- natgateway/main.tf
+++ natgateway/main.tf
@@ -27,20 +27,19 @@ resource "aws_route_table" "myroutetablenew" {
     tags = {
       Name=var.network_info.routablename
     }
-    depends_on = [ aws_nat_gateway.mynategateway,aws_vpc.mynatvpc ]
+    
 }
 resource "aws_nat_gateway" "mynategateway" {
     connectivity_type = "private"
     subnet_id = aws_subnet.mynatsubnets[0].id
     count = local.pub_subnet_value
-    depends_on = [ aws_subnet.mynatsubnets ]
+    
   
 }
 resource "aws_route_table_association" "myrouteassociation" {
     route_table_id = aws_route_table.myroutetablenew[0].id
     subnet_id = aws_subnet.mynatsubnets[1].id
     count = local.pub_subnet_value
-    depends_on = [  aws_route_table.myroutetablenew,aws_subnet.mynatsubnets]
 
 
   
