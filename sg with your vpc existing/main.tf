48b888f51d1ed1f86a025b883fe910dff5611b58
Marysrivallikalidindi <srivalli.kalidindi2004@gmail.com>
2026-02-24 23:41:29 +0530

    secruity grp with our created vpc

---
+++ sg with your vpc existing/main.tf
@@ -0,0 +1,23 @@
+data "aws_vpc" "myexistingvpc"{
+    filter {
+      name="tag:Name"
+      values = [ myvpc ]
+    }
+}
+resource "aws_security_group" "mysg1"{
+    name = "myopensg"
+    description="all networks ports allowing"
+    vpc_id=data.aws_vpc.myexistingvpc.id
+
+
+}
+resource "aws_vpc_security_group_ingress_rule" "myingressrule"{
+    security_group_id = aws_security_group.mysg1.id
+    from_port = 22
+    to_port = 22
+    ip_protocol = "tcp"
+    cidr_ipv4 = "0.0.0.0/0"
+
+
+
+}
