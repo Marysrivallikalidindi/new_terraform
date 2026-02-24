1738f6c20494fcfc445e17dd22c1af5c41883140
Marysrivallikalidindi <srivalli.kalidindi2004@gmail.com>
2026-02-24 22:54:54 +0530

    security group file added

---
+++ securitygrp/main.tf
@@ -0,0 +1,20 @@
+data "aws_vpc" "myexistingvpc"{
+    default=true
+}
+resource "aws_security_group" "mysg1"{
+    name = "myopensg"
+    description="all networks ports allowing"
+    vpc_id=data.aws_vpc.myexistingvpc.id
+
+
+}
+resource "aws_vpc_security_group_ingress_rule" "myingressrule"{
+    security_group_id = aws_security_group.mysg1.id
+    from_port = 22
+    to_port = 22
+    ip_protocol = "tcp"
+    cidr_ipv4 = "0.0.0.0/0"
+
+
+
+}
