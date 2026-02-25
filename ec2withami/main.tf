f9dfc1c2a03de419806e3873a57d5c874bbca7b1
Marysrivallikalidindi <srivalli.kalidindi2004@gmail.com>
2026-02-25 23:06:46 +0530

    provider file added

--- ec2withami/main.tf
+++ ec2withami/main.tf
@@ -18,8 +18,16 @@ resource "aws_key_pair" "myownkeypair" {
     public_key = file("~/id_ed25519.pub")
 
 }
+data "aws_ami" "myami" {
+    filter{
+        name="name"
+        values = ["ubuntu-eks-pro/k8s_1.31/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+    }
+    owners=["099720109477"]
+  
+}
 resource "aws_instance" "myec2" {
-    ami = "ami-051a31ab2f4d498f5"
+    ami = data.aws_ami.myami.id
     instance_type = "t3.micro"
     key_name = aws_key_pair.myownkeypair.key_name
     associate_public_ip_address = true
