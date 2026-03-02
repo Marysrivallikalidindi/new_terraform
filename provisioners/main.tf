62c143a23926ce6043e7fedc9599291ee2fd2eb0
Marysrivallikalidindi <srivalli.kalidindi2004@gmail.com>
2026-03-02 11:46:33 +0530

    provisioners inside main.tf file updated

--- provisioners/main.tf
+++ provisioners/main.tf
@@ -17,6 +17,15 @@ resource "aws_vpc_security_group_ingress_rule" "myingressrule"{
     ip_protocol = "tcp"
     cidr_ipv4 = "0.0.0.0/0"
 }
+resource "aws_vpc_security_group_ingress_rule" "myingressrule2" {
+  security_group_id = aws_security_group.mysg1.id
+  from_port         = 80
+  to_port           = 80
+  ip_protocol       = "tcp"
+  cidr_ipv4         = "0.0.0.0/0"
+}
+
+
 resource "aws_vpc_security_group_egress_rule" "allow_all_outbound" {
   security_group_id = aws_security_group.mysg1.id
   ip_protocol       = "-1"
@@ -51,6 +60,7 @@ resource "aws_instance" "myec2" {
       host = aws_instance.myec2.public_ip
     }
     provisioner "remote-exec" {
+
         inline = [ "sudo apt update","mkdir folder1","sudo apt install nginx -y" ]
       
     }
