93d4b746f8dfbc9c92f34a3dcb21fba6640a7bf4
Marysrivallikalidindi <srivalli.kalidindi2004@gmail.com>
2026-03-02 10:42:17 +0530

    provisioners added

---
+++ provisioners/main.tf
@@ -0,0 +1,50 @@
+data "aws_vpc" "myexistingvpc"{
+    default=true
+}
+resource "aws_security_group" "mysg1"{
+    name = "myopensg"
+    description="all networks ports allowing"
+    vpc_id=data.aws_vpc.myexistingvpc.id
+}
+resource "aws_vpc_security_group_ingress_rule" "myingressrule"{
+    security_group_id = aws_security_group.mysg1.id
+    from_port = 22
+    to_port = 22
+    ip_protocol = "tcp"
+    cidr_ipv4 = "0.0.0.0/0"
+}
+resource "aws_key_pair" "myownkeypair" {
+    key_name = "myownsshkey"
+    public_key = file("~/id_ed25519.pub")
+
+}
+data "aws_ami" "myami" {
+    filter{
+        name="name"
+        values = ["ubuntu-eks-pro/k8s_1.31/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-20251115"]
+    }
+    owners=["099720109477"]
+  
+}
+resource "aws_instance" "myec2" {
+    ami = data.aws_ami.myami.id
+    instance_type = "t3.micro"
+    key_name = aws_key_pair.myownkeypair.key_name
+    associate_public_ip_address = true
+    vpc_security_group_ids = [ aws_security_group.mysg1.id ]
+    tags = {
+      Name = "myterraformec2"
+    }
+    connection {
+      type = "ssh"
+      user = "ubuntu"
+      private_key = file("~./id_ed25519")
+      host = aws_instance.myec2.public_ip
+    }
+    provisioner "remote-exec" {
+        inline = [ "sudo apt update","mkdir folder1","sudo apt install nginx -y" ]
+      
+    }
+
+  
+}
