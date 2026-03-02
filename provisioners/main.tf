7a8f320c27ce64eca5dec950205e8ec69f1f5884
Marysrivallikalidindi <srivalli.kalidindi2004@gmail.com>
2026-03-02 11:54:10 +0530

    provisioners inside main.tf file updated

--- provisioners/main.tf
+++ provisioners/main.tf
@@ -24,7 +24,11 @@ resource "aws_vpc_security_group_ingress_rule" "myingressrule2" {
   ip_protocol       = "tcp"
   cidr_ipv4         = "0.0.0.0/0"
 }
-
+resource "aws_vpc_security_group_egress_rule" "allow_all_outbound" {
+  security_group_id = aws_security_group.mysg1.id
+  ip_protocol       = "-1"
+  cidr_ipv4         = "0.0.0.0/0"
+}
 resource "aws_key_pair" "myownkeypair" {
     key_name = "myownsshkey"
     public_key = file("~/id_ed25519.pub")
