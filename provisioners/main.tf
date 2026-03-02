bf56e6627063758dbb615a936b00f23cdfa5ae09
Marysrivallikalidindi <srivalli.kalidindi2004@gmail.com>
2026-03-02 12:01:59 +0530

    provisioners inside main.tf file updated

--- provisioners/main.tf
+++ provisioners/main.tf
@@ -24,6 +24,7 @@ resource "aws_vpc_security_group_ingress_rule" "myingressrule2" {
   ip_protocol       = "tcp"
   cidr_ipv4         = "0.0.0.0/0"
 }
+
 resource "aws_vpc_security_group_egress_rule" "allow_all_outbound" {
   security_group_id = aws_security_group.mysg1.id
   ip_protocol       = "-1"
