f078651dbd82d21922c92dde26ba507053b75505
Marysrivallikalidindi <srivalli.kalidindi2004@gmail.com>
2026-03-03 11:49:27 +0530

    webapplication.sh file modified

--- provthree/main.tf
+++ provthree/main.tf
@@ -63,10 +63,6 @@ resource "aws_instance" "myec2" {
         source = "mary.jpeg"
         destination = "/tmp/mary.jpeg"      
     }
-    provisioner "remote-exec" {
-        inline = [ "sudo chmod +x webapplication.sh","webapplication.sh" ]
-      
-    }
     provisioner "local-exec" {
         command = "mkdir demo"
       
