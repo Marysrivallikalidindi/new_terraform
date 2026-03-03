727bbade0956a8711b06b4344605cd7696176de0
Marysrivallikalidindi <srivalli.kalidindi2004@gmail.com>
2026-03-03 11:09:45 +0530

    provisoner file update

--- provthree/main.tf
+++ provthree/main.tf
@@ -64,7 +64,7 @@ resource "aws_instance" "myec2" {
     }
     provisioner "file" {
         source = "Mary_srivalli jpg (1).jpeg"
-        destination = "tmp/Mary_srivalli jpg (1).jpeg"      
+        destination = "/tmp/Mary_srivalli jpg (1).jpeg"      
     }
     provisioner "remote-exec" {
         inline = [ "sudo chmod +x /home/ubuntu/webapplication.sh","./webapplication.sh" ]
